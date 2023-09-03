384b6303cabe7bea5e44e8568f84efc287bc2988
Mudassir <mudassirinoxent@gmail.com>
2023-09-03 18:41:59 +0500

    fetch api get method

--- lec(fetch_API).js
+++ lec(fetch_API).js
@@ -1,8 +1,17 @@
-let p = fetch("");
-p.then((val)=>{
-    return val.json();
+// let p = fetch("https://goweather.herokuapp.com/weather/Ny");
+var result;
+let p = fetch("https://jsonplaceholder.typicode.com/posts")
+p.then((response)=>{
+    console.log(response.status);
+    console.log(response.ok);
+    console.log(response.headers);
+    return response.json();
 }).then((val2)=>{
-    return val2.json();
+    console.log(val2)
+    result = val2;
 }).catch((err)=>{
     console.log(err)
 })
+
+let w = document.querySelector(".weather")
+// w.innerHTML = "The waether is : " + result;
